956430d08cd5163ac563cc6d07e0ddfc0b37bccd
Jason <320175912@qq.com>
2019-05-23 01:53:34 +0800

    增加+4

---
+++ app/src/main/java/com/keyuanc/jason/jasonlog/MainActivity.java
@@ -0,0 +1,13 @@
+package com.keyuanc.jason.jasonlog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
